b84d274ef836fb6f11a17c078d7d3e0c4bd49cfd
Manoj Kumar <cs5180411@iitd.ac.in>
2021-04-22 01:32:51 +0530

    binary search continued

--- Makefile
+++ Makefile
@@ -1,10 +1,21 @@
 linearsearchobjects = buffer_manager.o file_manager.o linearsearch.o
+binarysearchobjects = buffer_manager.o file_manager.o binarysearch.o
 
 linearsearch : $(linearsearchobjects)
 	g++ -std=c++11 -o linearsearch $(linearsearchobjects)
 
 linearsearch.o : linearsearch.cpp
 	g++ -std=c++11 -c linearsearch.cpp
+
+binarysearch : $(binarysearchobjects)
+	g++ -std=c++11 -o binarysearch $(binarysearchobjects)
+
+binarysearch.o : binarysearch.cpp
+	g++ -std=c++11 -c binarysearch.cpp
+
+brun :
+	rm output.txt
+	./binarysearch TestCases/TC_search/sorted_input TestCases/TC_search/query_search.txt output.txt
 
 buffer_manager.o : buffer_manager.cpp
 	g++ -std=c++11 -c buffer_manager.cpp
@@ -15,3 +26,4 @@ file_manager.o : file_manager.cpp
 clean :
 	rm -f *.o
 	rm -f linearsearch
+	rm -f binarysearch
--- binarysearch.cpp
+++ binarysearch.cpp
@@ -5,223 +5,64 @@
 #include <fstream>
 #include <limits.h>
 
-int MINUS_ONE = -1;
-int MINUS_INF = INT_MIN;
-bool debug_mode = false;
-
-void printfile(FileHandler fh, string s){
-    cout << "-------------------------- " << s << " --------------------------\n";
-    cout << "\t";
-    for(int j = 0; j < PAGE_CONTENT_SIZE/4; j++) {
-        cout << "Offset"<< j << "\t"; 
-    }
-    cout << endl;
-    int x = fh.FirstPage().GetPageNum();
-    int y = fh.LastPage().GetPageNum();
-    fh.UnpinPage(x);
-    fh.FlushPage(x);
-    fh.UnpinPage(y);
-    fh.FlushPage(y);
-    for(int i=x; i<=y; i++){
-        PageHandler ph = fh.PageAt(i);
-        char *data = ph.GetData();
-        cout << "Page" << i << "\t";
-        for(int j = 0; j < PAGE_CONTENT_SIZE/4; j++) {
-            int val;
-            memcpy(&val, &data[j*4], sizeof(int));     
-            if(val==INT_MIN){
-                cout << "-Inf\t";
-            }
-            else{
-                cout << val  << "\t";            
-            }
-        }
-        fh.UnpinPage(i);
-        fh.FlushPage(i);
-        cout << endl;
-    }
-    cout << "-----------------------------------------------------------------\n";
-}
-
-int no_of_elements_in_lastpage(FileHandler fh){
-    int l = fh.LastPage().GetPageNum();
-    char * data = fh.PageAt(l).GetData();
-    fh.UnpinPage(l);
-    fh.FlushPage(l);
-    int i;
-    for(i=0; i<PAGE_CONTENT_SIZE/4; i++){
-        int val;
-        memcpy(&val, &data[i*4], sizeof(int));
-        if(val==INT_MIN){
-            break;
-        }
-    }
-    return i;
-}
-
-
-void printchar(char *c, int k){
-    if(k==1)
-    cout << "CHAR1 = ";
-    else
-    cout << "CHAR2 = ";
-    for(int i=0; i<PAGE_CONTENT_SIZE/4; i++){
-        cout << (int)(c[i*4]) << " ";
-    }
-    cout << endl;
-}
-
-
 int main(int argc, const char* argv[]){
-    ifstream inputFile;
-	inputFile.open(argv[2]);
+    if(argc <4)
+    {
+        cout<<"Few arguments"<<endl;
+        return 0;
+    }
+    FileManager fm;
+    FileHandler in = fm.OpenFile(argv[1]);
+    FileHandler out = fm.CreateFile(argv[3]);
 
-	FileManager fm;
-	FileHandler fh1 = fm.OpenFile(argv[1]);
-    FileHandler fh2 = fm.CreateFile(argv[3]);
-	int page_count = -1;
-    int entries = 0;
-	string search_tag;
-    int num;
-    int page_number_flush;
+    int totalPage = in.LastPage().GetPageNum()+1;
+    int intPerPage = PAGE_CONTENT_SIZE/sizeof(int);
     
-    int firstpage = fh1.FirstPage().GetPageNum();
-    int lastpage = fh1.LastPage().GetPageNum();
-    int total_nos = lastpage*(PAGE_CONTENT_SIZE/4) + no_of_elements_in_lastpage(fh1);
+    char* data = in.LastPage().GetData();
+    int intLastPage = 0;
+    int curr;
+    int i=-4;
+    while(intLastPage < intPerPage)
+    {
+        memcpy(&curr, &data[i+sizeof(int)], sizeof(int));
+        if(curr == INT_MIN)
+            break;
+        intLastPage++;
+        i = i+4;
+    }
+    int totalnumbers = (totalPage-1)*intPerPage+intLastPage;
 
-    fh1.UnpinPage(firstpage);
-    fh1.FlushPage(firstpage);
-    fh1.UnpinPage(lastpage);
-    fh1.FlushPage(lastpage);
-
-    if(debug_mode) printfile(fh1,"INPUT FILE");
-	while(inputFile >> search_tag){
-        inputFile>>num;
-		if(debug_mode) cout << "Query Search Number: " << num << endl;
+    ifstream query;
+    query.open(argv[2]);
+    string temp;
+    while(query >> temp)
+    {
+        int a;
+        query>>a;
         int start = 0;
-        int end = total_nos-1;
-        int first_occurence=-1;
-        int last_occurence=-1;
+        int end = totalnumbers-1;
+
+        while(start <= end)
+        {
+            int mid = start + (end-start)/2;
+            int pageIndex = mid/intPerPage;
+            int offset = mid%intPerPage;
+            data = in.PageAt(pageIndex).GetData();
+
+            memcpy(&curr, &data[offset*sizeof(int)], sizeof(int));
+            int prev = INT_MIN;
+            if(mid > 0)
+            {
+                if(offset != 0)
+                    memcpy(&prev, &data[(offset-1)*sizeof(int)], sizeof(int));
+                else {
+                    in.UnpinPage(pageIndex);
+                    in.FlushPage(pageIndex);
+                    data = in.PageAt(pageIndex-1).GetData();
+                    memcpy(&prev, &data[intPerPage*sizeof(int)], sizeof(int));
+                }
+            }
+        }
         
-        while(start<=end){
-            int val, val_;
-            int mid = start + (end-start)/2;
-            int pg = mid/(PAGE_CONTENT_SIZE/4);
-            int offset = mid%(PAGE_CONTENT_SIZE/4);             
-            char *data1 = fh1.PageAt(pg).GetData();
-
-            fh1.UnpinPage(pg);
-            fh1.FlushPage(pg);
-
-            memcpy(&val, &data1[offset*4], sizeof(int));
-            int pg_ =-1, offset_=-1; 
-            if(mid>0){
-                pg_ = (mid-1)/(PAGE_CONTENT_SIZE/4);
-                offset_ = (mid-1)%(PAGE_CONTENT_SIZE/4);
-                char *data1_ = fh1.PageAt(pg_).GetData();
-                memcpy(&val_, &data1_[offset_*4], sizeof(int));
-                fh1.UnpinPage(pg_);
-                fh1.FlushPage(pg_);
-            }
-            if((mid==0 || num>val_) && val==num){
-                first_occurence = mid;
-                break;
-            }
-            else if(num>val) start = mid+1;
-            else end = mid - 1;
-        }
-
-        start = 0;
-        end = total_nos-1;
-
-        while(start<=end){
-            int val, val_;
-            int mid = start + (end-start)/2;
-            int pg = mid/(PAGE_CONTENT_SIZE/4);
-            int offset = mid%(PAGE_CONTENT_SIZE/4);             
-            char *data1 = fh1.PageAt(pg).GetData();
-            fh1.UnpinPage(pg);
-            fh1.FlushPage(pg);
-            memcpy(&val, &data1[offset*4], sizeof(int));
-            int pg_ =-1, offset_=-1; 
-            if(mid<total_nos-1){
-                pg_ = (mid+1)/(PAGE_CONTENT_SIZE/4);
-                offset_ = (mid+1)%(PAGE_CONTENT_SIZE/4);
-                char *data1_ = fh1.PageAt(pg_).GetData();
-                fh1.UnpinPage(pg_);
-                fh1.FlushPage(pg_);
-                memcpy(&val_, &data1_[offset_*4], sizeof(int));
-            }
-            if((mid==total_nos-1 || num<val_) && val==num){
-                last_occurence = mid;
-                break;
-            }
-            else if(num<val) end = mid - 1;
-            else start = mid + 1;
-        }
-
-        // cout << first_occurence << ":" << last_occurence << endl;
-        // fm.PrintBuffer();
-        for(int i=first_occurence; i<=last_occurence; i++){
-            if(i==-1) break;
-            entries++;
-            int x = i / (PAGE_CONTENT_SIZE/4);
-            int y = i % (PAGE_CONTENT_SIZE/4);
-            if(debug_mode) cout << "FOUND " << num << " AT (" << x << "," << y << ")" << endl;
-            PageHandler ph2;
-            if(entries/(PAGE_CONTENT_SIZE/4)>page_count){
-                page_count++;
-                ph2 = fh2.NewPage();
-            }
-            else{
-                ph2 = fh2.LastPage();
-            }
-            int offset = (entries-1)%(PAGE_CONTENT_SIZE/4);
-            char* data2 = ph2.GetData();
-            memcpy(&data2[offset*4], &x, sizeof(int));
-            entries++;
-            offset = (entries-1)%(PAGE_CONTENT_SIZE/4);
-            memcpy(&data2[offset*4], &y, sizeof(int));   
-            page_number_flush = ph2.GetPageNum();
-            fh2.MarkDirty(page_number_flush);
-            fh2.UnpinPage(page_number_flush);
-            fh2.FlushPage(page_number_flush);
-        }
-        entries++;
-        PageHandler ph2;
-        if(entries/(PAGE_CONTENT_SIZE/4)>page_count){
-            page_count++;
-            ph2 = fh2.NewPage();
-        }
-        else{
-            ph2 = fh2.LastPage();
-        }
-        int offset = (entries-1)%(PAGE_CONTENT_SIZE/4);
-        char* data2 = ph2.GetData();        
-        memcpy(&data2[offset*4], &MINUS_ONE, sizeof(int));
-        entries++;
-        offset = (entries-1)%(PAGE_CONTENT_SIZE/4);
-        memcpy(&data2[offset*4], &MINUS_ONE, sizeof(int)); 
-        page_number_flush = ph2.GetPageNum();
-        fh2.MarkDirty(page_number_flush);
-        fh2.UnpinPage(page_number_flush);
-        fh2.FlushPage(page_number_flush);
     }
-    int empty_spaces = (page_count+1)*(PAGE_CONTENT_SIZE/4)-entries;
-    
-    for(int i=0; i<empty_spaces; i++){
-        entries++;
-        PageHandler ph2 = fh2.LastPage();
-        char* data2 = ph2.GetData();
-        int offset = (entries-1)%(PAGE_CONTENT_SIZE/4);
-        memcpy(&data2[offset*4], &MINUS_INF, sizeof(int));
-
-        page_number_flush = ph2.GetPageNum();
-        fh2.MarkDirty(page_number_flush);
-        fh2.UnpinPage(page_number_flush);
-        fh2.FlushPage(page_number_flush);
-    }
-    if(debug_mode) printfile(fh2,"OUTPUT FILE");
-    fm.CloseFile(fh1);
-    fm.CloseFile(fh2);
 }
