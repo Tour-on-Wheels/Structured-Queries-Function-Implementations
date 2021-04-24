d7ec720afe3110e976432c796b897ba1beff2c9e
Dipen Kumar <kr.dipen4@gmail.com>
2021-04-24 18:52:45 +0530

    error_out_done

--- join1.cpp
+++ join1.cpp
@@ -32,19 +32,19 @@ int main(int argc, const char* argv[]) {
     fh3 = fm.CreateFile(argv[3]);
     for(int i = 0; i < size1; i++) {
         ph1 = fh1.PageAt(i);
-        for(int a=0; a<PAGE_CONTENT_SIZE/sizeof(int); a++) {
-            if(((int *)ph1.GetData())[a] == INT_MIN) {
-                break;
-            } for(int j = 0; j < size2; j++) {
-                ph2 = fh2.PageAt(j);
-                for(int b=0; b<PAGE_CONTENT_SIZE/sizeof(int); b++) {
+        for(int j = 0; j < size2; j++) {
+            ph2 = fh2.PageAt(j);
+            for(int a=0; a<PAGE_CONTENT_SIZE/sizeof(int); a++) {
+                if(((int *)ph1.GetData())[a] == INT_MIN) {
+                    break;
+                } for(int b=0; b<PAGE_CONTENT_SIZE/sizeof(int); b++) {
                     if(((int *)ph2.GetData())[b] == INT_MIN) {
                         break;
                     } else if(((int *)ph2.GetData())[b] == ((int *)ph1.GetData())[a]) {
                         output_write(((int *)ph1.GetData())[a]);
                     }
-                } fh2.FlushPage(j);
-            }
+                } 
+            } fh2.FlushPage(j);
         } fh1.FlushPage(i);
     } output_close();
     fm.CloseFile(fh1);
